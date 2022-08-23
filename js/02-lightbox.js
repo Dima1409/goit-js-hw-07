e7ef0c95e29a02de72d77e68829cdd54df2af231
Dima <57713733+Dima1409@users.noreply.github.com>
2022-08-23 13:07:48 +0300

    hw7

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+{/* <a class="gallery__item" href="large-image.jpg">
+  <img class="gallery__image" src="small-image.jpg" alt="Image description" />
+</a> */}
+
+const galleryImageContainer = document.querySelector(".gallery");
+const markupImage = createImageElement(galleryItems);
+galleryImageContainer.insertAdjacentHTML('beforeend', markupImage);
+
+function createImageElement(params) {
+    return params.map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a>`  
+    }).join('')
+}
+
+
+const lightbox = new SimpleLightbox('.gallery a', { 
+    captionsData: 'alt',
+    overlayOpacity: 0.4,
+    captionDelay: 250,
+    captionPosition: 'bottom'
+});
+
 console.log(galleryItems);
